44aea813d58c742fb13b29c8a18c0c7b372c1eb5
pankshok <kulyov.pavel@gmail.com>
2015-05-27 04:32:09 +0300

    add requirements.txt and deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+PYTHON=./env/bin/python3
+PIP3=./env/bin/pip3
+VENV=./env
+
+rm -rf ${VENV}
+virtualenv env
+./env/bin/pip install -r ./requirements.txt
+
+${PIP3} install --editable .
+source ./env/bin/activate
+echo "Use following command to activate virtual environment:"
+echo "source ${VENV}/bin/activate"
